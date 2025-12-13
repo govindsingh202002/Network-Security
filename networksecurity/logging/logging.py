22d4367d4e4d89077a34f7045bf9edd7f71abd85
govindsingh202002 <govindsingh202002@gmail.com>
2025-12-14 00:08:49 +0530

    updating logging file code

---
+++ networksecurity/logging/logging.py
@@ -0,0 +1,33 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+from datetime import datetime
+
+#get directory of this logging file 
+base_dir= os.path.dirname(os.path.abspath(__file__))
+# define log folder directory path
+log_dir=os.path.join(base_dir,"..","..","logs")
+os.makedirs(log_dir,exist_ok=True)
+# define log file path name
+log_file_name=f"{datetime.now().strftime("%d_%m_%Y_%H_%M_%S")}.log"
+log_file_path=os.path.join(log_dir,log_file_name)
+
+# creating a log formatter
+log_formatter=logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+#create rotating file handler 
+file_handler=RotatingFileHandler(
+    filename=log_file_path,
+    maxBytes=5*1024*1024,
+    backupCount=5,
+    encoding="utf-8"
+)
+file_handler.setFormatter(log_formatter)
+file_handler.setLevel(logging.DEBUG)
+
+#create logger
+
+logger=logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(file_handler)
+logger.propagate=False
